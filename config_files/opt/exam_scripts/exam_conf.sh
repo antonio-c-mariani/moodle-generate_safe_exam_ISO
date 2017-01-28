381e6e11aecf90b5cbf6d8cf1cc35cbd14ec0bb1
Antonio Carlos Mariani <antonio.c.mariani@gmail.com>
2017-01-28 12:44:26 -0200

    Minor changes in the exam_conf

--- config_files/opt/exam_scripts/exam_conf.sh
+++ config_files/opt/exam_scripts/exam_conf.sh
@@ -3,10 +3,13 @@
 # ------------------------------------------------------------------
 # Session 1: General 
 # ------------------------------------------------------------------
-exam_version='3.3'			    # Version of this CD
+exam_version='3.3'		# Version of this CD
 
-exam_domain_cfg="inf.ufsc.br"               # Where to find de exam domain list (DNS RR TXT 'examdomains')
-                                            # Each domains must have an exam configuration url (DNS RR TXT 'examcfg')
+exam_domain_cfg="ddddd.com"	# Where to find de exam domain list (DNS RR TXT 'examdomains') similar to:
+				# ddddd.com. 3600 IN TXT "examdomains=ddddd.com eeeee.com fffff.com"
+				#	
+                                # Each domain must have an exam configuration url (DNS RR TXT 'examcfg') similar to:
+				# ddddd.com. 3600 IN TXT "examcfg=https://exams.ddddd.com/local/exam_authorization/config.php"
 
 ip_for_check="150.162.1.33"                 # Used for ping tests
 hostname_for_check="www.brasil.gov.br"      # Used for DNS resolution tests
@@ -14,11 +17,10 @@ hostname_for_check="www.brasil.gov.br"      # Used for DNS resolution tests
 # ------------------------------------------------------------------
 # Session 2: Institution data
 # ------------------------------------------------------------------
-exam_domain=""
-exam_description="Moodle Exams"
-institution_name="My Institution"
-institution_acronym="MyIt"
-contact_email="contact@my-institution.org"
+exam_description="Moodle Exams" 		# Showed in the screen background
+institution_name="My Institution"		# Showed in the screen background
+institution_acronym="MyIt"			# Showed in the screen background
+contact_email="contact@my-institution.org"	# Showed in the screen background
 exam_server_url="https://exam-myinstitution.org" # URL of the Moodle Exam
 
 # Extra hosts (ips) and TCP ports to be accessed (need to be open on the local firewall)
@@ -43,6 +45,7 @@ rulesv6="/etc/iptables/rules.v6"
 params_cfg=${scripts_dir}/exam_params.sh
 select_domain_limit=10 # After this time (minutes) a new domain can no longer be selected
 send_data_path="/local/exam_authorization/receive_data.php"
+exam_domain=""
 server_ip=""
 
 source ${params_cfg}
